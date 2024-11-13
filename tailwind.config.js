bde374b91cbaa058fed1c52e4197c473ac594f5c
NityanandaBehera <nityanandabehera85132@gmail.com>
2024-11-13 11:51:47 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path based on your project structure
+  ],
+  theme: {},
+  plugins: [],
+};
