42a9d785d42b2b7a05065af8f13c97e77f127bf8
Rahul Kumar <rahulkumar880476@gmail.com>
2024-11-13 17:21:33 +0530

    Screen 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
+import People from "./pages/People";
 // import Settings from "./pages/Settings";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/people" element={<People />} />
           {/* <Route path="/settings" element={<Settings />} /> */}
           <Route path="*" element={<Dashboard />} /> {/* Default route */}
         </Routes>
