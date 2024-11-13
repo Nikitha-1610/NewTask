a8435768308aaf165bcb3fed718be86fa2d9fc7b
NityanandaBehera <nityanandabehera85132@gmail.com>
2024-11-13 12:32:13 +0530

    dashboad

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
-  return <h1 className=" text-2xl font-semibold text-center">welcome </h1>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+// import Settings from "./pages/Settings";
+
+const App = () => {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          {/* <Route path="/settings" element={<Settings />} /> */}
+          <Route path="*" element={<Dashboard />} /> {/* Default route */}
+        </Routes>
+      </Layout>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+// import BarChart from "../components/BarChart";
+// import ComplexTable from "../components/ComplexTable";
+// import HalfPieChart from "../components/ProgressPieChart";
+// import ProjectChart from "../components/ProjectChart";
+// import TeamsData from "../components/TeamsData";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Dashboard body</h2>
+      {/* <TeamsData />
+      <BarChart />
+      <div className=" flex gap-4 w-full mt-3">
+        <HalfPieChart />
+        <div className=" w-full">
+          <ComplexTable />
+        </div>
+      </div>
+      <ProjectChart /> */}
+    </div>
+  );
+};
+
+export default Dashboard;
