5013ba5a268db518041c072bcc97774bb419fff7
NityanandaBehera <nityanandabehera85132@gmail.com>
2024-11-26 11:36:09 +0530

    fixed

--- src/pages/DesignTeam.jsx
+++ src/pages/DesignTeam.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCalendarAlt,
-  faUsers,
   faComment,
   faSliders,
   faPlus,
