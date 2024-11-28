f67faf6a1ffe1bf7a39a716f7a52d823c777c2fa
DhineshVenu <dhineshvenu01@gmail.com>
2024-11-28 11:53:31 +0530

    save

--- src/pages/addTasks.jsx
+++ src/pages/addTasks.jsx
@@ -16,6 +16,8 @@ const AddTasks = () => {
     referenceFileUrl: [], // Reference as an array
   });
 
+  const [isPopupVisible, setIsPopupVisible] = useState(false); //popup
+
   const [showUserList, setShowUserList] = useState(false);
   const [selectedUserType, setSelectedUserType] = useState("");
   const [users, setUsers] = useState([]);
@@ -128,6 +130,9 @@ const AddTasks = () => {
           referenceFileUrl: [],
         });
         setDisplayReferences([]);
+
+        // show popup after successful submission
+        setIsPopupVisible(true);
       } else {
         console.error("Unexpected response:", response);
         // toast.error("Something went wrong. Please try again.");
@@ -138,8 +143,39 @@ const AddTasks = () => {
     }
   };
 
+
+
   return (
     <div className="w-full p-6">
+     {/* Popup */}
+     {isPopupVisible && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white rounded-lg p-6 shadow-lg relative w-11/12 md:w-1/3">
+            {/* Close Icon */}
+            <button
+              className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+              onClick={() => setIsPopupVisible(false)}
+            >
+              <Icon icon="mdi:alpha-x-circle" height={24} width={24} />
+            </button>
+            {/* Popup Content */}
+            <div className="text-center">
+              <h2 className="text-xl font-bold text-gray-800">
+                Task Added Successfully!
+              </h2>
+              <p className="text-gray-600 mt-2">
+                Your task has been successfully added to this project.
+              </p>
+              <button
+                onClick={() => setIsPopupVisible(false)}
+                className="mt-6 bg-teal-500 text-white px-6 py-2 rounded-full"
+              >
+                Great
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="flex md:justify-end justify-center mb-5">
         <div className="flex flex-wrap justify-end gap-4">
           {/* Add Task Button */}
