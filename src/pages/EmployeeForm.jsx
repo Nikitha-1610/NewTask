9da0ca6ca80f62e68aded86a7af7e41f092b7cef
NityanandaBehera <nityanandabehera85132@gmail.com>
2024-11-29 08:30:17 +0530

    teams

--- src/pages/EmployeeForm.jsx
+++ src/pages/EmployeeForm.jsx
@@ -199,7 +199,7 @@ const EmployeeForm = () => {
             </label>
             <input
               type="text"
-              name="useremail"
+              name="email"
               value={formData.email}
               onChange={handleInputChange}
               className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-teal-500"
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -109,12 +109,12 @@ const Teams = () => {
             }}
           >
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+              className="bg-blue-500 text-white px-2 py-1 text-sm rounded-md"
               onClick={handleMessageButtonClick}
             >
               Message
             </button>
-            <button className="bg-green-500 text-white px-4 py-2 rounded-md">
+            <button className="bg-green-500 text-white px-2 py-1 text-sm rounded-md">
               Status
             </button>
           </div>
