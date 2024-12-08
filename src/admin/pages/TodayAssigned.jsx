f2ef431d51cb4cae8c8bf684624dea313ebb2426
manasa <piratimanasa9@gmail.com>
2024-12-08 08:25:31 +0530

    added drag and drop functionality for cards in taskpage

--- src/admin/pages/TodayAssigned.jsx
+++ src/admin/pages/TodayAssigned.jsx
@@ -1,12 +1,12 @@
 import { useLocation } from "react-router-dom";
 import TaskDetails from "../components/TaskDetails";
 import { Icon } from "@iconify/react";
-
 import { useNavigate } from "react-router-dom";
-const InProgressTasks = () => {
+const InTest = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
+  console.log("here is location", location);
 
   const tasksFromState = location.state || [];
 
@@ -21,14 +21,15 @@ const InProgressTasks = () => {
     comments: task.comment || [],
     taskDescription: task.taskDescription,
   }));
+
   const goBack = () => {
     navigate("/admin/task");
   };
 
   return (
-    <div className=" space-y-5">
+    <div className="space-y-5">
       <div className="flex items-center justify-between pb-4 border-gray-300">
-        <div className=" flex gap-2">
+      <div className=" flex gap-2">
           <button
             onClick={goBack}
             style={{
@@ -40,25 +41,25 @@ const InProgressTasks = () => {
           >
             <Icon icon="mdi:arrow-left" height={24} width={24} />
           </button>
-          <h2 className="text-xl font-bold bg-yellow-200 px-2 p-1 rounded-md text-gray-700">
-            In Progress({sampleTask.length})
-          </h2>
+        <h2 className="text-xl font-bold bg-red-300 px-2 p-1 rounded-md text-gray-700">
+          TodayAssigned ({sampleTask.length})
+        </h2>
         </div>
-        {/* <div className=" flex gap-3">
+        {/* <div className="flex gap-3">
           <button className="px-3 py-2 text-sm text-white bg-teal-500 rounded hover:bg-teal-600">
             + Add a task
           </button>
-          <h2 className=" flex gap-2">
+          <h2 className="flex gap-2">
             <Icon icon="lets-icons:filter" height={22} width={22} />
             Filter
           </h2>
         </div> */}
       </div>
       {sampleTask.map((task) => (
-        <TaskDetails key={task.id} task={task} className=" gap-6" />
+        <TaskDetails key={task.id} task={task} className="gap-6" />
       ))}
     </div>
   );
 };
 
-export default InProgressTasks;
+export default InTest;
