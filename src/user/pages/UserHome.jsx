73c0b93d30fb6c0ebaea250b4030ea6ecc73b874
manasa <piratimanasa9@gmail.com>
2024-12-09 19:20:21 +0530

    smallchange in homepage

--- src/user/pages/UserHome.jsx
+++ src/user/pages/UserHome.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+  import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCalendarAlt,
@@ -10,7 +10,8 @@ import {
   faLink,
 } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+
+import ReactLoading from 'react-loading';
 
 const DateDisplay = ({ isoDate }) => {
   if (!isoDate) return "No Date";
@@ -45,6 +46,7 @@ const UserHome = () => {
   const [filterLabel, setFilterLabel] = useState("");
   const [showFilterDropdown, setShowFilterDropdown] = useState(false);
   const [collapsedColumns, setCollapsedColumns] = useState({});
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const baseUrl = "https://3qhglx2bhd.execute-api.us-east-1.amazonaws.com/";
 
@@ -63,6 +65,8 @@ const UserHome = () => {
         }
       } catch (error) {
         console.error("Error fetching tasks:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -137,7 +141,6 @@ const UserHome = () => {
           DESIGN TEAM
         </h1>
         <div className="flex space-x-4 flex-wrap items-center sm:ml-auto sm:space-x-4 md:space-x-6">
-          
           <div className="relative">
             <button
               onClick={() => setShowFilterDropdown(!showFilterDropdown)}
@@ -169,94 +172,100 @@ const UserHome = () => {
         </div>
       </div>
 
-      {/* Board Columns */}
-      <div className="flex flex-col md:flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
-        {filteredColumns.map((column, colIndex) => (
-          <div
-            key={colIndex}
-            className="flex-2 w-full sm:w-full md:w-full lg:basis-1/3 lg:max-w-lg"
-          >
-            <div className="flex items-center justify-between border-b-2 pb-2">
-              <button
-                className={`font-semibold p-2 ${column.color}-600 flex-grow`}
-              >
-                {column.title}
-              </button>
-              <span className="flex items-center justify-center w-6 h-6 bg-gray-200 text-xs rounded-full ml-auto">
-                {column.tasks.length}
-              </span>
-              <div className="lg:hidden">
-                <FontAwesomeIcon
-                  icon={
-                    collapsedColumns[colIndex] ? faChevronDown : faChevronUp
-                  }
-                  className="ml-5 cursor-pointer"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    toggleColumn(colIndex);
-                  }}
-                />
-              </div>
-            </div>
-
-            {(!collapsedColumns[colIndex] || window.innerWidth >= 1024) && (
-              <div className="mt-4">
-                {column.tasks.map((task, taskIndex) => (
-  <div
-    key={taskIndex}
-    className="block bg-white shadow rounded-lg p-4 mb-4 relative border border-gray-400 w-full"
-  >
-    <div className="absolute top-2 right-2">
-      {task.taskStatus === "Completed" ? (
-        <div className="flex items-center text-green-500 text-xs font-bold">
-          <span className="mr-1">✔✔</span>
-          <span>Done</span>
+      {/* Loader */}
+      {loading ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <ReactLoading type="spin" color="#00bfae" height={50} width={50} />
         </div>
       ) : (
-        <div className="flex items-center text-gray-500 text-sm">
-          <FontAwesomeIcon
-            icon={faCalendarAlt}
-            className="mr-1"
-          />
-          <DateDisplay isoDate={task.deadline} />
+        // Board Columns
+        <div className="flex flex-col md:flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
+          {filteredColumns.map((column, colIndex) => (
+            <div
+              key={colIndex}
+              className="flex-2 w-full sm:w-full md:w-full lg:basis-1/3 lg:max-w-lg"
+            >
+              <div className="flex items-center justify-between border-b-2 pb-2">
+                <button
+                  className={`font-semibold p-2 ${column.color}-600 flex-grow`}
+                >
+                  {column.title}
+                </button>
+                <span className="flex items-center justify-center w-6 h-6 bg-gray-200 text-xs rounded-full ml-auto">
+                  {column.tasks.length}
+                </span>
+                <div className="lg:hidden">
+                  <FontAwesomeIcon
+                    icon={
+                      collapsedColumns[colIndex] ? faChevronDown : faChevronUp
+                    }
+                    className="ml-5 cursor-pointer"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      toggleColumn(colIndex);
+                    }}
+                  />
+                </div>
+              </div>
+
+              {(!collapsedColumns[colIndex] || window.innerWidth >= 1024) && (
+                <div className="mt-4">
+                  {column.tasks.map((task, taskIndex) => (
+                    <div
+                      key={taskIndex}
+                      className="block bg-white shadow rounded-lg p-4 mb-4 relative border border-gray-400 w-full"
+                    >
+                      <div className="absolute top-2 right-2">
+                        {task.taskStatus === "Completed" ? (
+                          <div className="flex items-center text-green-500 text-xs font-bold">
+                            <span className="mr-1">✔✔</span>
+                            <span>Done</span>
+                          </div>
+                        ) : (
+                          <div className="flex items-center text-gray-500 text-sm">
+                            <FontAwesomeIcon
+                              icon={faCalendarAlt}
+                              className="mr-1"
+                            />
+                            <DateDisplay isoDate={task.deadline} />
+                          </div>
+                        )}
+                      </div>
+                      {task.taskName && (
+                        <span
+                          className={`text-xs font-semibold mb-2 mt-4 inline-block px-2 py-1 rounded ${generateRandomColor()}`}
+                        >
+                          {task.taskName}
+                        </span>
+                      )}
+                      <div className="flex justify-between">
+                        <div>
+                          <div className="text-sm text-black-100">
+                            {task.taskDescription || "No description available."}
+                          </div>
+                          <div className="flex items-center text-sm text-gray-600 mt-2 space-x-4">
+                            <div className="flex items-center">
+                              <FontAwesomeIcon
+                                icon={faComment}
+                                className="mr-1"
+                              />
+                              {task.comment?.length || 0} Comments
+                            </div>
+                            <div className="flex items-center">
+                              <FontAwesomeIcon icon={faLink} className="mr-1" />
+                              {task.attachments?.length || 0} Attachments
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       )}
-    </div>
-    {task.taskName && (
-      <span
-        className={`text-xs font-semibold mb-2 mt-4 inline-block px-2 py-1 rounded ${generateRandomColor()}`}
-      >
-        {task.taskName}
-      </span>
-    )}
-    <div className="flex justify-between">
-      <div>
-        <div className="text-sm text-black-100">
-          {task.taskDescription || "No description available."}
-        </div>
-        <div className="flex items-center text-sm text-gray-600 mt-2 space-x-4">
-          <div className="flex items-center">
-            <FontAwesomeIcon
-              icon={faComment}
-              className="mr-1"
-            />
-            {task.comment?.length || 0} Comments
-          </div>
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faLink} className="mr-1" />
-            {task.attachments?.length || 0} Attachments
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-))}
-
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
